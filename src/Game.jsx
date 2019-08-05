c986c1951d8a1551acbe130d27871fff510fd867
Jonathan Meyer <jon@stej.com>
2019-08-05 16:30:23 -0400

    stating some testing

--- src/Game.jsx
+++ src/Game.jsx
@@ -8,15 +8,14 @@ class Game extends React.Component {
   }
 
   render() {
-    const { title, description, logo, images } = this.props;
+    const { title, logo, images } = this.props;
 
     return (
       <div className="game">
         <h1>{title}</h1>
+        <p>{logo}</p>
         <ul>
-          {images.map((image, key) => (
-            <li key={key}>{image}</li>
-          ))}
+          {images && images.map((image, key) => <li key={key}>{image}</li>)}
         </ul>
         <pre>{JSON.stringify(images, null, 2)}</pre>
       </div>
