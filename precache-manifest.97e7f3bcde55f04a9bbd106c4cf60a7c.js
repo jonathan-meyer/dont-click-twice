0bd06de5f12a5e6ef2a9f20c57b717be1388c5d7
Jonathan Meyer <jon@stej.com>
2019-08-05 17:06:51 -0400

    Updates

---
+++ precache-manifest.97e7f3bcde55f04a9bbd106c4cf60a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a544f4a6324eede578f21c4da098bed4",
+    "url": "/dont-click-twice/index.html"
+  },
+  {
+    "revision": "9e78a4a5cf2989281a7c",
+    "url": "/dont-click-twice/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1893dd1e813636335428",
+    "url": "/dont-click-twice/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9e78a4a5cf2989281a7c",
+    "url": "/dont-click-twice/static/js/2.3ec7d0b8.chunk.js"
+  },
+  {
+    "revision": "1893dd1e813636335428",
+    "url": "/dont-click-twice/static/js/main.7f798240.chunk.js"
+  },
+  {
+    "revision": "0e166a852a7764d3efa5",
+    "url": "/dont-click-twice/static/js/runtime~main.b513dcf3.js"
+  }
+]);
