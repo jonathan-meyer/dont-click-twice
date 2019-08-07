f5ca52a8debfd86aa06b87bbcaf877cf332a89df
Jonathan Meyer <jon@stej.com>
2019-08-07 15:12:10 -0400

    updated image scraper

--- src/scrapeImages.js
+++ src/scrapeImages.js
@@ -1,29 +1,56 @@
 const fs = require("fs");
 const path = require("path");
 const superagent = require("superagent");
-var mime = require("mime-types");
+const mime = require("mime-types");
 
-const data = require("./src/data.json");
+const data = require("../src/data.json");
+
+const images = data =>
+  Object.keys(data).map(key => data[key].images.map(image => require(image)));
 
 const getImage = (url, name) =>
   superagent.get(url).then(res => {
     const ext = mime.extension(res.headers["content-type"]);
-    const file = path.resolve("public", "images", `${name}.${ext}`);
-
-    console.log({ url: url.toJSON, file });
+    const folder = path.resolve(__dirname, "..", "public");
+    const file = path.resolve(path.resolve(folder, "images"), `${name}.${ext}`);
 
     fs.writeFileSync(file, res.body);
+
+    return path.relative(folder, file);
   });
 
-Object.keys(data).map(key => {
-  const { logo, images } = data[key];
+const manually = data =>
+  Promise.all(
+    Object.keys(data).map(key => {
+      const { logo, images } = data[key];
 
-  Promise.all([
-    getImage(logo, `${key}-logo`),
-    ...images.map((image, i) =>
-      getImage(image, `${key}-image-${i.toString().padStart(2, "0")}`)
-    )
-  ])
-    .then(() => console.log("done"))
-    .catch(err => console.log({ err }));
-});
+      return Promise.all([
+        getImage(logo, `${key}-logo`),
+        ...images.map((image, i) =>
+          getImage(image, `${key}-image-${i.toString().padStart(2, "0")}`)
+        )
+      ]).then(result =>
+        result.reduce((p, c, i) => {
+          if (i === 0) {
+            p = { key, logo: c, images: [] };
+          } else {
+            p.images.push(c);
+          }
+
+          return p;
+        }, {})
+      );
+    })
+  ).then(result =>
+    result.reduce((p, c) => ({ ...p, [c.key]: { ...c, key: undefined } }), {})
+  );
+
+manually(data)
+  .then(result => {
+    console.log({ result });
+    fs.writeFileSync(
+      path.resolve(__dirname, "..", "public", "images.json"),
+      JSON.stringify(result, null, 2)
+    );
+  })
+  .catch(err => console.log(err));
